d96195ce89e39c74e165f7f41423a93831ce349c
Voinash Denys <desdenisandqwerty@gmail.com>
2023-12-28 21:44:00 +0200

    fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,6 +5,7 @@ const Statistics = () => (
     <h2 class="title">Upload stats</h2>
 
     <ul class="stat-list">
+      {}
       <li class="item">
         <span class="label">.docx</span>
         <span class="percentage">4%</span>
