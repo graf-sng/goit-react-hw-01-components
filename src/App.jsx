a6a681de97fd3dabc4612476e5ef192522ace6fb
Voinash Denys <desdenisandqwerty@gmail.com>
2023-12-28 21:56:23 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 
-import user from '../data/user.json';
-import data from '../data/data.json';
+import user from './data/user.json';
+import data from './data/data.json';
 
 const App = () => {
   return (
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,27 +1,16 @@
 import css from './Statistics.module.css';
 
-const Statistics = () => (
+const Statistics = ({ title, stats }) => (
   <section class="statistics">
-    <h2 class="title">Upload stats</h2>
+    <h2 class="title">{title}</h2>
 
-    <ul class="stat-list">
-      {}
-      <li class="item">
-        <span class="label">.docx</span>
-        <span class="percentage">4%</span>
-      </li>
-      <li class="item">
-        <span class="label">.mp3</span>
-        <span class="percentage">14%</span>
-      </li>
-      <li class="item">
-        <span class="label">.pdf</span>
-        <span class="percentage">41%</span>
-      </li>
-      <li class="item">
-        <span class="label">.mp4</span>
-        <span class="percentage">12%</span>
-      </li>
+    <ul class={css.statList}>
+      {stats.map(({ id, label, percentage }) => (
+        <li class="item" key={id}>
+          <span class={css.label}>{label} </span>
+          <span class={css.percentage}>{percentage}</span>
+        </li>
+      ))}
     </ul>
   </section>
 );
